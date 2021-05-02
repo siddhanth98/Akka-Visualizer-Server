3dc20148dda7e7769846948aecbe972e029752f7
siddhanth98 <svenka42@uic.edu>
2021-05-02 04:48:41 -0500

    Uses an event priority queue to defer handling of out-of-order events.

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ const {createEdge, removeEdge, resetEdgeIndex} = require("./models/EdgeOperation
 const app = express();
 
 let actorHandlerClient, visualizerClient;
+let events = []; /* this will store socket events with millisecond-timestamps */
 
 app.use(express.json());
 app.use(index);
@@ -30,32 +31,64 @@ io.on("connection", (socket) => {
 
     console.log("client connected");
 
-    function handleNodeCreation(obj) {
-        console.log(obj);
+    function handleEvent(ev) {
+        switch(ev["eventName"]) {
+            case "constructNode":
+                handleNodeCreation(ev);
+                break;
+            case "constructEdge":
+                handleEdgeCreation(ev);
+                break;
+            case "deleteNode":
+                handleNodeDeletion(ev);
+                break;
+        }
+    }
+
+    function assessEvent(eventName, obj) {
         let o = JSON.parse(obj);
+        let currentEventTime = o["time"];
+
+        if (events.length > 0) {
+            let ev = JSON.parse(events[0]);
+            let latestEventTime = ev["time"];
+
+            if (latestEventTime <= currentEventTime) {
+                /* Current event is in order */
+                /* Remove and handle stored event first and then store current event to be handled later */
+                console.log(`current event ${eventName}(t=${currentEventTime}) is in order with stored event ${ev["eventName"]}(t=${latestEventTime})`);
+                events.shift();
+                handleEvent(ev);
+            }
+        }
+        /* Current event happened earlier than latest event stored */
+        /* Forward current event to vis */
+        console.log(`current event ${eventName}(t=${currentEventTime}) is inserted in the queue now`);
+        o["eventName"] = eventName;
+        events.push(JSON.stringify(o));
+    }
+
+    function handleNodeCreation(o) {
+        console.log(o);
         let newNode = createNode(o.name);
         if (visualizerClient) visualizerClient.emit("constructNode", newNode.id, o.name);
     }
 
-    function handleEdgeCreation(obj) {
-        console.log(obj);
-        let o = JSON.parse(obj);
+    function handleEdgeCreation(o) {
+        console.log(o);
         let label = o.label, from = getNodeId(o.from), to = getNodeId(o.to);
         console.log(`edge \"${label}\" constructed from ${o.from}(id=${from}) to ${o.to}(id=${to})\n`);
 
         let edgeId = removeEdge(from, to);
         let newEdge = createEdge(from, to, label);
 
-        if (edgeId !== -1 && visualizerClient) {
+        if (edgeId !== -1 && visualizerClient)
             visualizerClient.emit("deleteEdge", edgeId);
-            return;
-        }
         if (visualizerClient)
             visualizerClient.emit("constructEdge", newEdge.id, label, from, to);
     }
 
-    function handleNodeDeletion(obj) {
-        let o = JSON.parse(obj);
+    function handleNodeDeletion(o) {
         let nodeId = deleteNode(o.name);
         console.log(`killed ${o.name}`);
         if (visualizerClient) visualizerClient.emit("deleteNode", nodeId);
@@ -75,17 +108,17 @@ io.on("connection", (socket) => {
             }
         })
         .on("constructNode", (obj) => {
-            handleNodeCreation(obj);
+            assessEvent("constructNode", obj);
         })
         .on("constructEdge", (obj) => {
-            handleEdgeCreation(obj);
+            assessEvent("constructEdge", obj);
         })
         .on("destroyNode", (obj) => {
-            handleNodeDeletion(obj);
+            assessEvent("deleteNode", obj);
         })
 /*
         .on("setState", (state) => {
-            let nodeId = update(state);
+            update(state);
             if (visualizerClient) visualizerClient.emit("setState", state);
         })
 */
